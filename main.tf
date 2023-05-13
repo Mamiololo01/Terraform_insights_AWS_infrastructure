dc1bbe01aa2d3950537e71d62f0bd62ffb5e6136
Christopher Ozurumba <67044030+Mamiololo01@users.noreply.github.com>
2023-05-13 07:58:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5YX5BMRR7CDKH7US"
-  secret_key = "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"
+  access_key = "AKIA5YX5BMRR7CDK43327US"
+  secret_key = "Cp2492T55nzLVDKlIpagewBa/vwjR1EDFrTQR1Iimc"
 }
